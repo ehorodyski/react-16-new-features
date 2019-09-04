2aad830828e9e2591e93fa677f16a751bc1fa910
Eric Horodyski <eric@horodyski.me>
2019-09-04 12:03:54 -0400

    Add NoteApp component.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,45 @@ const App = (props) => {
 };
 App.defaultProps = { count: 0, label: 'count' };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NoteApp = () => {
+
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([...notes, { title, body }]);
+    setTitle('');
+    setBody('');
+  };
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <p>Add note:</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button>Add Note</button>
+      </form>
+      {notes.map((note, index) => (
+        <div key={index}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+ReactDOM.render(<NoteApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
